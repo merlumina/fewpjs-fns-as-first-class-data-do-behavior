765e3069cf1b341961d99ac3111f79f392524530
Liz Wilson <lizriderwilson@gmail.com>
2021-06-15 13:03:44 -0400

    commit tree 869052ae9d0f40c02123d145324b82683251fccc

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const hour = parseInt(time.split(":")[0]);
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = message;
+}
